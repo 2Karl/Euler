d93e2707bfe9d57bd7c912d1b82862530795bd69
ka-smith <ka.smith@warriner.oxon.sch.uk>
2021-10-21 11:22:21 +0100

    Updated probelms

--- 19 counting sundays.py
+++ 19 counting sundays.py
@@ -1,4 +1,14 @@
-days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-year = 1900
-month = 0
-
+count = 0
+day = 7
+for year in range(1901, 2000):
+    leap = year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+    days = [31, [28, 29][leap], 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    month = 0
+    while month < 12:
+        day += 7
+        if day > days[month]:
+            day -= days[month]
+            month += 1
+        if day == 1:
+            count += 1
+print(count)
