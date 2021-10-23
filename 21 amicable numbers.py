eda885a138849007c3667b8436aa8d51c99d4590
2Karl Tessier-Ashpool <gosub1250@gmail.com>
2021-10-23 13:36:47 +0100

    Tidied up amicable numbers

--- 21 amicable numbers.py
+++ 21 amicable numbers.py
@@ -1,47 +1,5 @@
-# def prime_sieve(max_n):
-#     numbers = [2] + list(range(3, max_n + 1, 2))
-#     primes = []
-#     while numbers[0] <= max_n**0.5:
-#         primes.append(numbers[0])
-#         numbers = [number for number in numbers if number % numbers[0] != 0]
-#     primes += numbers
-#     return primes
-#
-# def is_prime(n):
-#     return n in primes
-#
-# def amicable(n, p, q, r):
-#     return (2**n*p*q, 2**n*r)
-#
-# primes = prime_sieve(1000000)
-#
-#
-# for n in range(1, 1000):
-#     print("N: " + str(n))
-#     for m in range(1, n):
-#         p = (2**(n-m)+1)*2**m-1
-#         if is_prime(p):
-#
-#             q = (2**(n-m)+1)*2**n-1
-#             if is_prime(q):
-#
-#                 r=(2**(n-m)+1)**2*2**(m+n)-1
-#                 if is_prime(r):
-#
-#                     print(n, m)
-#                     print(amicable(n, p, q, r))
-
 def sum_factors(n):
-    factors = [1]
-    for i in range(2, int(n**0.5) + 1):
-        if n%i ==0:
-            factors.extend([i, n//i])
-    return sum(list(set(factors)))
+    return sum(sum([i, n // i]) for i in range(2, int(n ** 0.5) + 1) if n % i == 0) + 1
 
-amicable = []
-for i in range(1, 10000):
-    if sum_factors(sum_factors(i)) == i and sum_factors(i) != i:
-        print(i, sum_factors(i), sum_factors(sum_factors(i)))
-        amicable.append(i)
 
-print(sum(amicable))
+print(sum(i for i in range(220, 10000) if sum_factors(sum_factors(i)) == i != sum_factors(i)))
