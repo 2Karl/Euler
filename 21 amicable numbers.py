89586df88c970b73947fff0226f5565949192194
2Karl Tessier-Ashpool <gosub1250@gmail.com>
2021-10-23 10:12:15 +0100

    trash

--- 21 amicable numbers.py
+++ 21 amicable numbers.py
@@ -1,31 +1,47 @@
-def prime_sieve(max_n):
-    numbers = [2] + list(range(3, max_n + 1, 2))
-    primes = []
-    while numbers[0] <= max_n**0.5:
-        primes.append(numbers[0])
-        numbers = [number for number in numbers if number % numbers[0] != 0]
-    primes += numbers
-    return primes
-
-def is_prime(n):
-    return n in primes
-
-def amicable(n, p, q, r):
-    return (2**n*p*q, 2**n*r)
-
-primes = prime_sieve(1000000)
-
-
+# def prime_sieve(max_n):
+#     numbers = [2] + list(range(3, max_n + 1, 2))
+#     primes = []
+#     while numbers[0] <= max_n**0.5:
+#         primes.append(numbers[0])
+#         numbers = [number for number in numbers if number % numbers[0] != 0]
+#     primes += numbers
+#     return primes
+#
+# def is_prime(n):
+#     return n in primes
+#
+# def amicable(n, p, q, r):
+#     return (2**n*p*q, 2**n*r)
+#
+# primes = prime_sieve(1000000)
+#
+#
 # for n in range(1, 1000):
-#   for m in range(1, n):
-#     p = (2**(n-m)+1)*2**m-1
-#     if is_prime(p):
+#     print("N: " + str(n))
+#     for m in range(1, n):
+#         p = (2**(n-m)+1)*2**m-1
+#         if is_prime(p):
 #
-#         q = (2**(n-m)+1)*2**n-1
-#         if is_prime(q):
+#             q = (2**(n-m)+1)*2**n-1
+#             if is_prime(q):
 #
-#             r=(2**(n-m)+1)**2*2**(m+n)-1
-#             if is_prime(r):
+#                 r=(2**(n-m)+1)**2*2**(m+n)-1
+#                 if is_prime(r):
 #
-#                 print(n, m)
-#                 print(amicable(n, p, q, r))
+#                     print(n, m)
+#                     print(amicable(n, p, q, r))
+
+def sum_factors(n):
+    factors = [1]
+    for i in range(2, int(n**0.5) + 1):
+        if n%i ==0:
+            factors.extend([i, n//i])
+    return sum(list(set(factors)))
+
+amicable = []
+for i in range(1, 10000):
+    if sum_factors(sum_factors(i)) == i and sum_factors(i) != i:
+        print(i, sum_factors(i), sum_factors(sum_factors(i)))
+        amicable.append(i)
+
+print(sum(amicable))
